a47c2033cbf5d2e8cbeaf76a8160f42da1083396
Andrew Hunter <andrew.hunter@red-gate.com>
2013-12-18 16:48:28 +0000

    Use the syncing that we've got already to load the search page from the database instead of the server (this isn't really syncing though as it still won't work when the server is offline)

--- Client/ViewModels/SearchPageViewModel.js
+++ Client/ViewModels/SearchPageViewModel.js
@@ -1,6 +1,8 @@
-﻿var SearchPageViewModel = function(server, user, password) {
-    this.customerArray = ko.observableArray([]);
+﻿var SearchPageViewModel = function (server, user, password) {
+    var that = this;
+    that.customerArray = ko.observableArray([]);
     
+    /*
     ajax(server + '/api/customers/', user, password, 'GET', {
         context: this,
         success: function (data) {
@@ -16,10 +18,27 @@
             this.customerArray(mappedData);
         }
     });
-
-    this.SelectedCustomer = ko.observable();
+    */
     
-    this.viewCustomer = function (customer) {
+    // TODO: send modified records first to stop them getting stomped on
+    sync.updateRecords(server, user, password, function() {
+        syncDataModel.retrieveAll(function(data) {
+            var mappedData = $.map(data, function (item) {
+                var fromJs = ko.mapping.fromJS(item);
+                fromJs['Notes'] = ko.observable(item['Notes']);
+                fromJs['DisplayName'] = ko.computed(function () {
+                    return fromJs.FirstName() + ' ' + fromJs.SecondName();
+                });
+                return fromJs;
+            });
+
+            that.customerArray(mappedData);
+        });
+    });
+
+    that.SelectedCustomer = ko.observable();
+    
+    that.viewCustomer = function (customer) {
         PageStateManager.changePage('viewPage.html', new ViewPageViewModel(server, user, password, customer));
     };
 };
